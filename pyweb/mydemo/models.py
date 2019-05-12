fdd982d971e6bf775872dcfe920392ed24eb58e2
fuxxkk <315768455@qq.com>
2019-05-12 19:24:27 +0800

    init

--- pyweb/mydemo/models.py
+++ pyweb/mydemo/models.py
@@ -4,10 +4,12 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     # id
-    id = models.IntegerField(primary_key=True, unique=True)
+    id = models.IntegerField(primary_key=True, unique=True, null=False)
     # 用户名
-    username = models.CharField(max_length=225)
+    username = models.CharField(max_length=225, unique=True, null=False)
     # 密码
     password = models.CharField(max_length=225)
 
-    create_date = models.DateField()
+    create_date = models.DateField(null=True)
+
+    modify_date = models.DateField()
