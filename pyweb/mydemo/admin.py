1304e51b8ecb149227979928b3f2e6cadbedbc2c
fuxxkk <315768455@qq.com>
2019-05-16 19:08:02 +0800

    update

--- pyweb/mydemo/admin.py
+++ pyweb/mydemo/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from mydemo.models import User
 
-admin.site.register(User)
+
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ["id","username"]
+    search_fields = ["id","username"]
+    list_per_page = 3
+
+
+admin.site.register(User,UserInfoAdmin)
