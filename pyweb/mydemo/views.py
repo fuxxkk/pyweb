3b6323995006f2679b30da180376007384399842
fuxxkk <315768455@qq.com>
2019-05-20 19:22:14 +0800

    update

--- pyweb/mydemo/views.py
+++ pyweb/mydemo/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
     return HttpResponse("hello world")
+
+
+def login(request):
+    return render(request, 'pages/login.html')
